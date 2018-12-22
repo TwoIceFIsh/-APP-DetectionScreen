4657b6e873a26b44c170d97a01da85b341ab82d4
twoicefish <twoicefish15@gmail.com>
2018-12-22 20:59:56 +0900

    Sample data model was created then was saved it

--- Test.py
+++ Test.py
@@ -1,2 +1,43 @@
 import tensorflow as tf
 import numpy as np
+import os
+
+x_data = np.array([[0, 0],
+                  [0, 1],
+                  [1, 0],
+                  [1, 1]], dtype=np.float32)
+y_data = np.array([[0],
+                  [0],
+                  [0],
+                  [1]], dtype=np.float32)
+
+
+X = tf.placeholder(tf.float32, [None, 2], name='x-input')
+Y = tf.placeholder(tf.float32, [None, 1], name='y-input')
+
+W = tf.Variable(tf.random_normal([2, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+# cost/loss function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    for step in range(10001):
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print("\nCorrect: ", c, "\nAccuracy: ", a)
+    saver.save(sess, os.path.join("model", "model"))
+    sess.close()
+
+

80f5a976ebabfe28e6ee262df68a08646145d353
twoicefish <twoicefish15@gmail.com>
2018-12-22 20:02:38 +0900

    Start Learning The Tensorflow

--- Test.py
+++ Test.py
@@ -1,20 +1,2 @@
-import win32gui
-
-
-def callback(hwnd, extra):
-    rect = win32gui.GetWindowRect("DeadByDaylight ")
-    x = rect[0]
-    y = rect[1]
-    w = rect[2] - x
-    h = rect[3] - y
-    print("Window %s:" % win32gui.GetWindowText(hwnd))
-    print("\Location: (%d, %d)" % (x, y))
-    print("\    Size: (%d, %d)" % (w, h))
-
-
-def main():
-    win32gui.EnumWindows(callback, None)
-
-
-if __name__ == '__main__':
-    main()
+import tensorflow as tf
+import numpy as np
